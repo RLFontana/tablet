8781bc4ef639f76aa3489bf9af18f96dec6a9eb5
Rafael Luiz <rafael.faesa@outlook.com>
2018-11-25 01:08:47 -0200

    Firebase

---
+++ app/src/main/java/br/com/easygo/cliente/firebase/MessagingService.java
@@ -0,0 +1,25 @@
+package br.com.easygo.cliente.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+    final String TAG = "MessagingService";
+
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        // Get updated InstanceID token.
+        String refreshedToken = s;
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "ID: " + remoteMessage.getCollapseKey());
+    }
+}
