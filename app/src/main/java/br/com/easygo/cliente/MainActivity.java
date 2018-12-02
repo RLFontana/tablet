c8ca02e283e7f921f9652f61c5425e9e9052d156
Guthierry <guthyalmeida@gmail.com>
2018-12-02 19:27:31 -0200

    Ajuste tela inicial

--- app/src/main/java/br/com/easygo/cliente/MainActivity.java
+++ app/src/main/java/br/com/easygo/cliente/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,7 +43,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
         InMemoryDB.fillObjects();
+
+        FloatingActionButton fabPedido = findViewById(R.id.fab_novo_pedido);
 
 
         BroadcastReceiver receiver = new BroadcastReceiver() {
@@ -65,9 +71,8 @@ public class MainActivity extends AppCompatActivity {
 
         InMemoryDB.currentGarcom = InMemoryDB.garcomDAO.get(0);
 
-        Button pedido = findViewById(R.id.button);
         final Context context = this;
-        pedido.setOnClickListener(new View.OnClickListener() {
+        fabPedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mesa = new Intent(MainActivity.this, MesaActivity.class);
